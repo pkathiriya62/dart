5cf9153cb0af8b5e5bcfaca45630dcf4ea55e708
Prince kathiriya <princekathiriya62@gmail.com>
2023-09-19 17:41:49 +0530

    dart

---
+++ arithmetic_operator.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  int a=10;
+  int b=20;
+  
+  var c=a+b;
+  var d=a*b;
+  var e=a/b;
+  var f=a-b;
+  var s=a%b;
+  
+  print(c);
+  print(d);
+  print(e);
+  print(f);
+  print(s);
+  
+}
