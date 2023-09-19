856a376013647552c1dd91d5465c7613caf80e93
Prince kathiriya <princekathiriya62@gmail.com>
2023-09-19 18:16:50 +0530

    dart

---
+++ relational_operator.dart
@@ -0,0 +1,63 @@
+void main()
+{
+  int a=50;
+  int b=40;
+  
+  if(a<b)
+  {
+    print("a is small.");
+  }
+  else
+  {
+    print("b is small.");
+  }
+  if(a>b)
+  {
+    print("a is big.");
+  }
+  else
+  {
+    print("b is big.");
+  }
+
+  if(a==b)
+  {
+    print("is equal.");
+  }
+  else
+  {
+    print("check value.");
+  }
+
+  if(a!=b)
+  {
+    print("is not equal to");
+  }
+  else
+  {
+    print("check value");
+  }
+
+  if(a<=b)
+  {
+    print("a is small.");
+  }
+  else
+  {
+    print("b is small.");
+  }
+
+  if(a>=b)
+  {
+    print("a is small.");
+  }
+  else
+  {
+    print("b is small.");
+  }
+
+
+
+
+  
+}
