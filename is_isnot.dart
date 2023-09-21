f2bcec5809720e05d655df130cea822796d3740f
Prince kathiriya <princekathiriya62@gmail.com>
2023-09-21 18:43:31 +0530

    dart

--- is_isnot.dart
+++
@@ -1,8 +0,0 @@
-void main()
-{
-  var a=10;
-  // ignore: unnecessary_type_check
-  print(a is! int);
-  // ignore: unnecessary_type_check
-  print(a is int);
-}
---
+++ stdinout.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+void main()
+{
+  print("enter your firstname");
+  String? firstname = stdin.readLineSync();
+
+
+  print("enter your lastname");
+  String? lastname = stdin.readLineSync();
+
+}
