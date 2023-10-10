2a37895938e5d0b4feafa82ed3f30ffa42a1e779
Prince kathiriya <princekathiriya62@gmail.com>
2023-10-10 18:46:26 +0530

    dart

---
+++ class_object.dart
@@ -0,0 +1,23 @@
+class car{
+  String? car_name;
+  String? car_model;
+  showcarinfo()
+  {
+    print("company:$car_name");
+    print("model:$car_model");
+  }
+
+}
+void main()
+{
+  car car1=new car();
+  car car2=new car();
+
+  car1.car_name="ford";
+  car1.car_model="endeavour";
+  car1.showcarinfo();
+
+  car2.car_name="hyundai";
+  car2.car_model="verna";
+  car2.showcarinfo();
+}
