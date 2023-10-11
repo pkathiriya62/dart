3e58dc8de13114e4ddc3e87523c2052b65824965
Prince kathiriya <princekathiriya62@gmail.com>
2023-10-11 17:59:05 +0530

    dart

---
+++ polymorphism.dart
@@ -0,0 +1,30 @@
+class Laptop
+{
+  display()
+  {
+    print('laptop display');
+  }
+}
+
+class MacBook extends Laptop {
+  void display()
+  {
+    print("MacBook");
+    super.display();
+  }
+}
+
+class Macbookpro extends Laptop
+{
+  void display()
+  {
+    print("Macbookpro");
+    super.display();
+  }
+}
+
+  void main() {
+  var macbookpro = Macbookpro();
+  macbookpro.display();
+
+}
