5f607641d0e1285447e678a7d1ab94706c173e42
Prince kathiriya <princekathiriya62@gmail.com>
2023-10-12 18:06:52 +0530

    dart

---
+++ records.dart
@@ -0,0 +1,19 @@
+void main()
+{
+  var record=('one','two','three');
+  var record1=('one',1,false,2.3);
+
+  print(record);
+  print(record1);
+
+  var data = ('name',b:1,c:5,'data');
+  print(data.$1);
+
+  (int,int,int)sum(int k,(int i,int j)r)
+  {
+    return(k,k,k);
+  }
+  
+  var sumis=sum(4,(4,5));
+  print(sumis);
+}
